725ff9fc29491c398cfa8b1e07c0aad80ad9c9ac
Seelam Sandeep reddy <2200030956@kluniversity.in>
2025-08-15 13:12:58 +0530

    Add files via upload

---
+++ Server/src/test/java/com/klef/Server/HandloomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandloomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
